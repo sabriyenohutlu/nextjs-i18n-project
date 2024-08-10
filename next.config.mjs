cd40631bced22590b0d7d563f938d04dc9c163c5
sabriyeuregen <sabriyeuregen@gmail.com>
2024-08-10 15:43:52 +0300

    I trying i18n in next.js 14.2

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    i18n: {
+        locales: ['en-US', 'fr', 'tr-TR'], // Desteklenen diller ve bölgeler
+        defaultLocale: 'en-US', // Varsayılan dil ve bölge
+      },
+};
 
 export default nextConfig;
