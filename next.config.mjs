b3d4c2f83d8a35bcf2dcdf29035530742c5c7a11
sabriyeuregen <sabriyeuregen@gmail.com>
2024-08-10 22:22:27 +0300

    Added :  en.json tr.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    i18n: {
-        locales: ['en-US', 'fr', 'tr-TR'], // Desteklenen diller ve bölgeler
-        defaultLocale: 'en-US', // Varsayılan dil ve bölge
-      },
+  i18n: {
+    locales: ["en", "tr", "ar"],
+    defaultLocale: "en",
+  },
 };
 
 export default nextConfig;
